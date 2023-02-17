ebe2e87c8d21067d0ec629c15ca10f94feb573ec
Cubigator <sergeychuprin2006@gmail.com>
2023-02-17 18:49:15 +0700

    fall

---
+++ Assets/Scripts/Fall.cs
@@ -0,0 +1,19 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Fall : MonoBehaviour
+{
+    private HingeJoint2D _joint;
+    private void Start()
+    {
+        _joint = GetComponent<HingeJoint2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _joint.enabled = false;
+        }
+    }
+}
